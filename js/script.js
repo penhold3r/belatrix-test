c5331d5b8f9798a11ce2e97e86b834b5674de7eb
Caro <caro@Nico.local>
2017-11-22 15:38:16 -0300

    commenting lines

--- js/script.js
+++ js/script.js
@@ -2,25 +2,25 @@
 {
 	const btn = document.querySelector('#btn');
 	btn.addEventListener('click', loadData, false);
-})();
+})(); //IIFE
 //
 function loadData(e)
 {
-	const ubigeos = 'ubigeos.txt';
-	e.preventDefault();
+	const ubigeos = 'ubigeos.txt'; //instance of raw data
+	e.preventDefault(); // just to be sure
 	//
-	fetch(ubigeos)
-		.then((resp) => resp.text())
-		.then((data) => data.split('\n'))
-		.then((data) => data.map((line) => line.replace(/[“”]+/g, '')))
-		.then((data) => data.map((line) => line.split('/')))
+	fetch(ubigeos) // load data
+		.then((resp) => resp.text()) // parse response to text
+		.then((data) => data.split('\n')) // create array by each line
+		.then((data) => data.map((line) => line.replace(/[“”]+/g, ''))) // strip quotation marks from lines
+		.then((data) => data.map((line) => line.split('/'))) // separate into single elements
 		.then((data) =>
 		{
-			let json = [];
+			let json = []; // new object
 			data.map((line) =>
 			{
-				json.push({
-					departamento: line[0].trim(),
+				json.push({ // for each line of arrays create a key-value object
+					departamento: line[0].trim(), // trimming...
 					provincia: line[1].trim(),
 					distrito: line[2].trim()
 				});
@@ -36,20 +36,22 @@ function loadData(e)
 					pr = line.provincia,
 					dt = line.distrito;
 				//
-				finalJson.push({
+				finalJson.push({ // separate name and code for each entry
 					departamento: (dp != '') ? {codigo: dp.substr(0, dp.indexOf(' ')), nombre: dp.substr(dp.indexOf(' ') +1)} : '',
 					provincia: (pr != '') ? {codigo: pr.substr(0, pr.indexOf(' ')), nombre: pr.substr(pr.indexOf(' ') +1)} : '',
 					distrito: (dt != '') ? {codigo: dt.substr(0, dt.indexOf(' ')), nombre: dt.substr(dt.indexOf(' ') +1)} : ''
 				});
 			});
-			console.log(finalJson);
+			console.log(finalJson); // final object ordered and labeled
 			return finalJson
 		})
 		.then((data) =>
 		{
+			// an array for each future table
 			let tables = [{departamento: []}, {provincia: []}, {distrito: []}];
 			data.map((line) =>
 			{
+				// filtering elemnts for table and labeling acordingly
 				if(line.provincia == '')
 				{
 					tables[0].departamento.push({
@@ -85,21 +87,21 @@ function loadData(e)
 //
 function buildTables(tables)
 {
-	const tablesWrapper = document.querySelector('#tables');
+	const tablesWrapper = document.querySelector('#tables'); 
 	//
 	tables.map((table) =>
 	{
-		for(let i in table)
+		for(let i in table) // creating a table for each array
 		{
 			let tableTitle = document.createElement('h2'),
 				tableEl = document.createElement('table'),
 				tableHeader = document.createElement('tr');
 			//
-			tableTitle.innerHTML = i;
+			tableTitle.innerHTML = i; // name extracted from array key
 			tablesWrapper.appendChild(tableTitle);
-			tableHeader.innerHTML = '<th>Código</th><th>Nombre</th><th>Código Padre</th><th>Descripción Padre</th>';
+			tableHeader.innerHTML = '<th>Código</th><th>Nombre</th><th>Código Padre</th><th>Descripción Padre</th>'; // constant header
 			tableEl.appendChild(tableHeader);
-			for(let j in table[i])
+			for(let j in table[i]) // populate rows with each entry
 			{
 				let row = table[i],
 					tr = document.createElement('tr');
@@ -109,7 +111,7 @@ function buildTables(tables)
 				tr.innerHTML += '<td>'+ row[j].descripcionPadre + '</td>';
 				tableEl.appendChild(tr);
 			}
-			tablesWrapper.appendChild(tableEl);
+			tablesWrapper.appendChild(tableEl); // insert table into DOM
 		}
 	});
 }
